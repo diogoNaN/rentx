1ec521a913a6d12d4432d0df4de0e2523eb6571a
Diogo Farkas <diogonan@protonmail.ch>
2022-01-15 15:29:58 -0300

    add car model and schema

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -1,8 +1,9 @@
 import { appSchema } from "@nozbe/watermelondb";
 
 import { userSchema } from "./user";
+import { carSchema } from "./car";
 
 export const schemas = appSchema({
-  version: 1,
-  tables: [userSchema],
+  version: 2,
+  tables: [userSchema, carSchema],
 });
